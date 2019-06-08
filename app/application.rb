737a8ee3215718991b504a328eae9c4dcd69034c
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-08 04:50:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       if item = @@items.find {|i| i.name == item_name}
         resp.write item.price
       else
-      
+
       end
       resp.write "Route not found"
       resp.status = 404
