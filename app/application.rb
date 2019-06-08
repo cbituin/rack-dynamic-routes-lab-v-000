06506423174e2bb88cf546dd87610825c272b7c2
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-08 04:47:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path.match(/item/)
 
-      item_name = req.path.split("/items/").last
+      item_name = req.path.split("/item/").last
       item = @@item.find {|i| i.name == item_name}
 
       resp.write item.name
