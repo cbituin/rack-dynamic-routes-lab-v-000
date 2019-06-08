455dd5e6cc63d981e875d5cea9ca1443d7204e89
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-08 04:43:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = Item.all.find {|i| i.name == item_name}
+      item = @@item.find {|i| i.name == item_name}
       binding.pry
       resp.write item.price
     else
