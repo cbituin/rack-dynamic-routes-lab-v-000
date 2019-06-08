1440402564202b26b003890198d1ad28a15ecf6c
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-08 04:45:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@item.find {|i| i.name == item_name}
-      resp.write item.price
+      resp.write item
     else
       resp.write "Route not found"
       resp.status = 404
